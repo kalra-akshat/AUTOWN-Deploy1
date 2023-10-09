da855c8b43359da2e48404581cf9d1c85d515854
Akshat Kalra <akshat4kalra@gmail.com>
2023-10-09 12:08:49 +0530

    changes in projects

--- src/Components/Overview.js
+++ src/Components/Overview.js
@@ -1,5 +1,6 @@
 import binocs from './Images/binocs.png'
 import './Styling/Overview.css'
+import trpohy from './Images/trophy-icon-5.jpg'
 
 function Overview() {
 
@@ -12,7 +13,15 @@ function Overview() {
                 <div className="main">
                     <img className="binoc" src={binocs}></img>
                     <div className="overview-text">
-                        <p>Welcome to BITS AUTown'23, proudly presented under the banner of CASE - Connected, Autonomous, Secured, and Electrified. As an innovative platform, we unite industry pioneers to showcase their cutting-edge products while fostering collaboration with our ingenious students. Embracing the CASE ethos, participants can engage in thrilling competitions within the realms of Connected technologies, Autonomous systems, Security protocols, and Electrification advancements. Join us in shaping the future, where industry excellence converges with student ingenuity. Explore, collaborate, and champion the CASE revolution at BITS AUTown'23!</p>
+                        <p>Welcome to BITS AUTown'23, proudly presented under the banner of CASE - Connected, Autonomous, Secured, and Electrified. As an innovative platform, we unite industry pioneers to showcase their cutting-edge products while fostering collaboration with our ingenious students.
+                        Embracing the CASE ethos, participants can engage in thrilling competitions within the realms of Connected technologies, Autonomous systems, Security protocols, and Electrification advancements. Join us in shaping the future, where industry excellence converges with student ingenuity. 
+                        <br></br> Explore, collaborate, and champion the CASE revolution at BITS AUTown'23!  <br></br><br></br> 
+                        Participation Open to All.<br></br>Exicting Cash Prize for the Winners !</p>
+                    
+                        <div className="cashprize">
+                            <img src={trpohy}></img>
+                            <div className='cash'>₹ 50,000</div>
+                        </div>
                     </div>
                 </div>
             </div>
